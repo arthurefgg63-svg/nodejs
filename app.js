e8b0cb1a8f123aac53bc66966001953c9d477721
Arthur <arthurefgg63@gmail.com>
2026-05-24 08:57:32 -0300

    app.js

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
+const express = require("express")
+const { GoogleGenerativeAI } = require("@google/generative-ai")
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
 
-// Use the router for handling routes
-app.use('/', indexRouter);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY)
 
-// Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+app.post("/chat", async (req, res) => {
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+    try {
+
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-flash"
+        })
+
+        const result = await model.generateContent(req.body.message)
+
+        res.json({
+            reply: result.response.text()
+        })
+
+    } catch(err) {
+
+        console.log(err)
+
+        res.json({
+            reply: "Erro"
+        })
+    }
+})
+
+app.listen(process.env.PORT || 3000)
